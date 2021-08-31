2ab51837f6221c3476a4685e2e64db57bd74b5d2
Jortz <johnnydortiz@gmail.com>
2021-08-31 15:20:26 -0400

    slidable wip

---
+++ chess/slidable.rb
@@ -0,0 +1,34 @@
+module Slidable
+    def moves
+        #initialize a return array
+        results = []
+
+        #call a method that returns an array of directions
+        directions = self.move_dirs
+
+        #for each of those directions, add the direction coordinates to our starting position
+        #repeatedly until before we hit a like-colored piece, until we hit an opponent piece,
+        # or until we're off the board and add all those moves to our return array
+        directions.each do |direction|
+            x , y = @position[0], @position[1]
+            until (x < 0 || x > 7 || y < 0 || y > 7)
+                x += direction[0]
+                y += direction[1]
+                if @board[x][y].is_a?(NullPiece)
+                    #space is empty
+                    results << [x,y]
+                elsif self.color == @board[x][y].color
+                    #space is blocked
+                    break
+                else
+                    #space is capturable
+                    results << [x,y]
+                    break
+                end
+            end
+        end
+        #return our array
+        results
+        
+    end
+end
